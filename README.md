3b8bd2612ca8dd571f2a47dd9cc3f1887788ea3a
谷子毅 <504305797@qq.com>
2022-03-17 21:01:37 +0800

    admin model and base model

--- README.md
+++
@@ -1,4 +0,0 @@
-# UniversalPracticeRepository
-
-#### 介绍
-回归初心，打造一款开源的通用题目仓库，以适用于不同情景。
---
+++ sherly-urp-model/src/main/java/com/guzi/upr/model/admin/Menu.java
@@ -0,0 +1,12 @@
+package com.guzi.upr.model.admin;
+
+import lombok.Data;
+
+/**
+ * @author 谷子毅
+ * @email guzyc@digitalchina.com
+ * @date 2022/3/17
+ */
+@Data
+public class Menu {
+}
