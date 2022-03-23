873d8544fc83d93adb5743bd2cf552252b873900
谷子毅 <504305797@qq.com>
2022-03-23 15:31:50 +0800

    完成sql拦截实现动态表名、库名 （还需优化）

--- sherly-urp-common/src/main/java/com/guzi/upr/config/MybatisPlusConfig.java
+++ sherly-urp-common/src/main/java/com/guzi/upr/config/MybatisPlusConfig.java
@@ -1,10 +1,15 @@
 package com.guzi.upr.config;
 
+
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.DynamicTableNameInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author 谷子毅
@@ -15,10 +20,30 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MybatisPlusConfig {
 
+    private final String DEFAULT_DB = "sherly.";
+
+    private final String OTHER_DB = "sherly.";
+
+    private List<String> DEFAULT_TABLE = new ArrayList<>();
+
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+
+        // 动态表名
+        DynamicTableNameInnerInterceptor dynamicTableNameInnerInterceptor = new DynamicTableNameInnerInterceptor();
+        dynamicTableNameInnerInterceptor.setTableNameHandler((sql, tableName) -> {
+            if (DEFAULT_TABLE.contains(tableName)) {
+                return DEFAULT_DB + tableName;
+            } else {
+                return OTHER_DB + tableName;
+            }
+        });
+        interceptor.addInnerInterceptor(dynamicTableNameInnerInterceptor);
+
+        // 分页
         interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+
         return interceptor;
     }
 }
