c126c264140f5360a9f3f6f7e8c161d79ccba567
谷子毅 <504305797@qq.com>
2022-03-26 20:52:59 +0800

    1.完成权限更新，权限删除

--- sherly-upr-admin/src/main/java/com/guzi/upr/controller/PermissionController.java
+++ sherly-upr-admin/src/main/java/com/guzi/upr/controller/PermissionController.java
@@ -2,13 +2,12 @@ package com.guzi.upr.controller;
 
 import com.guzi.upr.model.Result;
 import com.guzi.upr.model.dto.PermissionInsertDTO;
+import com.guzi.upr.model.dto.PermissionUpdateDTO;
 import com.guzi.upr.service.PermissionService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author 谷子毅
@@ -25,8 +24,22 @@ public class PermissionController {
 
     @PostMapping("/save/one")
     @ApiOperation("权限新增")
-    public Result saveOne(PermissionInsertDTO dto) {
+    public Result saveOne(@RequestBody PermissionInsertDTO dto) {
         permissionService.saveOne(dto);
+        return Result.success();
+    }
+
+    @PutMapping("/update/one")
+    @ApiOperation("权限更新")
+    public Result updateOne(@RequestBody PermissionUpdateDTO dto) {
+        permissionService.updateOne(dto);
+        return Result.success();
+    }
+
+    @DeleteMapping("/remove/one")
+    @ApiOperation("权限删除")
+    public Result removeOne(@RequestParam Long id) {
+        permissionService.removeOne(id);
         return Result.success();
     }
 }
---
+++ sherly-upr-admin/src/main/java/com/guzi/upr/model/dto/PermissionUpdateDTO.java
@@ -0,0 +1,28 @@
+package com.guzi.upr.model.dto;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * @author 谷子毅
+ * @date 2022/3/26
+ */
+@Data
+public class PermissionUpdateDTO {
+
+    /** 权限id */
+    @ApiModelProperty("权限id")
+    private Long permissionId;
+
+    /** 权限名 */
+    @ApiModelProperty("权限名")
+    private String permissionName;
+
+    /** 描述 */
+    @ApiModelProperty("描述")
+    private String description;
+
+    /** 父权限id */
+    @ApiModelProperty("父权限id")
+    private Long parentId;
+}
