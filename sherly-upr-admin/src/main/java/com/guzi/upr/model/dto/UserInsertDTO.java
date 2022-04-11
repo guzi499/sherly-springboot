98b67cf0fe824bd359d146fc4ba0718c5001da13
付东辉 <fstxizya@qq.com>
2022-04-11 11:17:50 +0800

    更新

--- sherly-upr-admin/src/main/java/com/guzi/upr/model/dto/UserInsertDTO.java
+++ sherly-upr-admin/src/main/java/com/guzi/upr/model/dto/UserInsertDTO.java
@@ -12,41 +12,51 @@ import java.util.List;
  */
 @Data
 public class UserInsertDTO {
-
+    /**
+     * 昵称
+     */
     @ApiModelProperty("昵称")
     private String nickname;
 
-
+    /**
+     * 姓名
+     */
     @ApiModelProperty("姓名")
     private String realName;
 
-
+    /**
+     * 手机号
+     */
     @ApiModelProperty("手机号")
     private String phone;
-
-
+    /**
+     * 密码
+     */
     @ApiModelProperty("密码")
     private String password;
 
-
+    /**
+     * 用户头像
+     */
     @ApiModelProperty("用户头像")
     private String avater;
 
-
+    /**
+     * 用户邮箱
+     */
     @ApiModelProperty("用户邮箱")
     private String email;
 
-
+    /**性别*/
     @ApiModelProperty("性别")
     private Integer gender;
-
+    /**部门id*/
     @ApiModelProperty("部门id")
     private Long deptId;
-
-
+    /**0超级管理员 1管理员 2普通用户*/
     @ApiModelProperty("0超级管理员 1管理员 2普通用户")
     private Integer identity;
-
+    /**角色id*/
     @ApiModelProperty("角色id")
     private List<Long> roleIds;
 
