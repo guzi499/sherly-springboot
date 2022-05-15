865ef9827a7953df9b9d522c38b14e2f1420c4fb
谷子毅 <504305797@qq.com>
2022-05-15 22:28:27 +0800

    springboot整合七牛云

---
+++ sherly-upr-admin/src/main/java/com/guzi/upr/controller/QiniuController.java
@@ -0,0 +1,40 @@
+package com.guzi.upr.controller;
+
+import com.guzi.upr.model.Result;
+import com.guzi.upr.service.QiniuService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * @author 谷子毅
+ * @date 2022/5/15
+ */
+@RestController
+@RequestMapping("/api/qiniu")
+@Api(tags = "七牛云相关")
+public class QiniuController {
+
+    @Autowired
+    private QiniuService qiniuService;
+
+
+    @ApiOperation("文件上传")
+    @PostMapping("/upload/one")
+    public Result upload(@RequestParam MultipartFile file){
+        qiniuService.upload(file);
+
+        return Result.success();
+    }
+
+    @ApiOperation("文件下载")
+    @GetMapping("/download/one")
+    public Result download(@RequestParam String fileName) throws UnsupportedEncodingException {
+
+        return Result.success(qiniuService.download(fileName));
+    }
+}
---
+++ sherly-upr-admin/src/main/java/com/guzi/upr/service/QiniuService.java
@@ -0,0 +1,55 @@
+package com.guzi.upr.service;
+
+import com.guzi.upr.config.QiniuConfig;
+import com.qiniu.http.Response;
+import com.qiniu.storage.Configuration;
+import com.qiniu.storage.Region;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.util.Auth;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * @author 谷子毅
+ * @date 2022/5/15
+ */
+@Service
+public class QiniuService {
+
+    @Autowired
+    private QiniuConfig qiniuConfig;
+
+    public void upload(MultipartFile file) {
+
+        Configuration cfg = new Configuration(Region.huanan());
+        UploadManager uploadManager = new UploadManager(cfg);
+
+        Auth auth = Auth.create(qiniuConfig.getAccessKey(), qiniuConfig.getSecretKey());
+        String upToken = auth.uploadToken(qiniuConfig.getBucket());
+
+        try {
+            Response response = uploadManager.put(file.getBytes(), file.getOriginalFilename(), upToken);
+            System.out.println(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String download(String fileName) throws UnsupportedEncodingException {
+        String domainOfBucket = qiniuConfig.getPreUrl();
+        String encodedFileName = URLEncoder.encode(fileName, "utf-8").replace("+", "%20");
+
+        String publicUrl = String.format("%s/%s", domainOfBucket, encodedFileName);
+
+        Auth auth = Auth.create(qiniuConfig.getAccessKey(), qiniuConfig.getSecretKey());
+
+        long expireInSeconds = 3600;
+
+        return auth.privateDownloadUrl(publicUrl, expireInSeconds);
+    }
+}
