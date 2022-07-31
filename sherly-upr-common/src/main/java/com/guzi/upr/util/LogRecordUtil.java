840ba308e9a98dcda611ada8ae029f80eb231b97
谷子毅 <504305797@qq.com>
2022-07-31 21:38:18 +0800

    解决切换租户事务冲突问题，解决无数据时内容列表报错问题

--- sherly-upr-common/src/main/java/com/guzi/upr/util/LogRecordUtil.java
+++ sherly-upr-common/src/main/java/com/guzi/upr/util/LogRecordUtil.java
@@ -3,12 +3,14 @@ package com.guzi.upr.util;
 import cn.hutool.extra.servlet.ServletUtil;
 import cn.hutool.http.useragent.UserAgent;
 import cn.hutool.http.useragent.UserAgentUtil;
-import com.guzi.upr.log.model.LoginLog;
 import com.guzi.upr.log.manager.LoginLogManager;
+import com.guzi.upr.log.model.LoginLog;
 import net.dreamlu.mica.ip2region.core.Ip2regionSearcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
@@ -26,6 +28,7 @@ public class LogRecordUtil {
     @Autowired
     private Ip2regionSearcher regionSearcher;
 
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     public void recordLoginLog(HttpServletRequest request, String username, Integer result, Integer type) {
         LoginLog loginLog = new LoginLog();
 
