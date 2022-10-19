e9ef96e56cffb71eb6e0b20c6d98aeff01af3c2c
谷子毅 <504305797@qq.com>
2022-10-19 18:20:29 +0800

    查看操作日志不予记录

--- sherly-admin/src/main/java/com/guzi/sherly/controller/OperationLogController.java
+++ sherly-admin/src/main/java/com/guzi/sherly/controller/OperationLogController.java
@@ -5,6 +5,7 @@ import com.guzi.sherly.model.Result;
 import com.guzi.sherly.model.dto.OperationLogPageDTO;
 import com.guzi.sherly.model.vo.OperationLogPageVO;
 import com.guzi.sherly.model.vo.OperationLogVO;
+import com.guzi.sherly.modules.log.annotation.SherlyLog;
 import com.guzi.sherly.modules.log.service.OperationLogService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -29,6 +30,7 @@ public class OperationLogController {
     @GetMapping("/list_page")
     @PreAuthorize("hasAnyAuthority('operation_log:list_page')")
     @ApiOperation("操作日志分页")
+    @SherlyLog(noRecord = true)
     public Result<PageResult<OperationLogPageVO>> listPage(OperationLogPageDTO dto) {
         return Result.success(operationLogService.listPage(dto));
     }
@@ -36,6 +38,7 @@ public class OperationLogController {
     @GetMapping("/get_one")
     @PreAuthorize("hasAnyAuthority('operation_log:get_one')")
     @ApiOperation("操作日志详情")
+    @SherlyLog(noRecord = true)
     public Result<OperationLogVO> getOne(@RequestParam Long logId) {
         return Result.success(operationLogService.getOne(logId));
     }
