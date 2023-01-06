f8eebd5390fe6691967a1fa05766ee3b1bb56499
谷子毅 <504305797@qq.com>
2023-01-06 23:28:12 +0800

    修复@Data警告

--- sherly-sdk/src/main/java/com/guzi/sherly/modules/log/dto/OperationLogSelfPageDTO.java
+++ sherly-sdk/src/main/java/com/guzi/sherly/modules/log/dto/OperationLogSelfPageDTO.java
@@ -2,11 +2,13 @@ package com.guzi.sherly.modules.log.dto;
 
 import com.guzi.sherly.common.model.PageQuery;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * @author 谷子毅
  * @date 2022/8/1
  */
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class OperationLogSelfPageDTO extends PageQuery {
 }
